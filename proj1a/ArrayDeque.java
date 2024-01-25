2188950572f9d34fbe5bb304be416e29ce250feb
MirRoR4s <790356373@qq.com>
2024-01-25 23:00:56 +0800

    thinking about the ArrayDeque

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -2,9 +2,128 @@
  * 1. 常量时间增删（调整大小除外）
  * 2. 常量时间获取指定索引元素
  * 3. 常量时间获取长度
- * 
+ * 4. 数组的起始长度为 8
+ * 5. 程序所用内存与项数成正比，对于长度 16 以上的数组，其使用率至少应该在 25 % 以上
+ *
  * @author 黄建涛
  */
-public class ArrayDeque {
-    
+public class ArrayDeque<T> {
+    /*
+     * get(i) = items[(nextFirst + 1 + i) % items.length]
+     * addLast(int x): items[nextLast] =
+     */
+    int nextFirst;
+    int nextLast;
+    private T[] items;
+    private int size;
+
+    public ArrayDeque() {
+        items = (T[]) new Object[8];
+        size = 0;
+        nextFirst = 0;
+        nextLast = items.length - 1;
+    }
+
+    /**
+     * 修改数组的总长度
+     * @param cap 修改后数组的总长度
+     */
+    private void reSize(int cap) {
+        T[] newArray = (T[]) new Object[cap];
+        System.arraycopy(items, 0, newArray, 0, size);
+        items = newArray;
+    }
+
+    /**
+     * 将 item 置于队首
+     * @param item
+     */
+    public void addFirst(T item) {
+        if (size == items.length) {
+            reSize(size * 2);
+        }
+        items[nextFirst] = item;
+        nextFirst = (nextFirst - 1 + items.length) % items.length;
+        size += 1;
+    }
+
+    /**
+     * 将 item 置于队尾
+     * @param item
+     */
+    public void addLast(T item) {
+        if (size == items.length) {
+            reSize(items.length * 2);
+        }
+        if (nextLast + nextFirst + 1 == items.length) {
+            nextFirst += 1;
+        }
+        items[nextLast] = item;
+        nextLast = (nextLast + 1) % items.length;
+        size += 1;
+    }
+
+    /**
+     * 判断队列是否为空
+     * @return 为空返回 true，否则返回 false
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * 获取队列大小
+     * @return 队列大小
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * 以空格为分隔符从头至尾打印队列
+     */
+    public void printDeque() {
+        for (int i = 0; i < size; i++) {
+            if (i != 0) {
+                System.out.print(" " + get(i));
+            }
+            else {
+                System.out.print(get(i));
+            }
+        }
+    }
+
+    /**
+     * 删除并返回队首元素
+     * @return 原队首元素
+     */
+    public T removeFirst() {
+        int tmp = (nextFirst + 1) % items.length;
+        T head = items[tmp];
+        items[tmp] = null;
+        nextFirst = tmp;
+        size = size - 1;
+        return head;
+    }
+
+    /**
+     * 删除并返回队尾元素
+     * @return 原队尾元素
+     */
+    public T removeLast() {
+        int tmp = (nextLast - 1) % items.length;
+        T tail = items[tmp];
+        items[tmp] = null;
+        size -= 1;
+        return tail;
+    }
+
+    /**
+     * 获取指定 index 索引处的元素
+     * @param index 索引
+     * @return  index 索引处对应的队列元素
+     */
+    public T get(int index) {
+        return items[(nextFirst + 1 + index) % items.length];
+    }
 }
