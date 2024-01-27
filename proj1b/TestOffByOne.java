c01a2a4f831790060194dcb2255ef632cb405e23
MirRoR4s <790356373@qq.com>
2024-01-27 20:07:34 +0800

    add upper or lower case test

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -16,5 +16,6 @@ public class TestOffByOne {
         assertFalse(offByOne.equalChars('a', 'e'));
         assertFalse(offByOne.equalChars('z', 'a'));
         assertFalse(offByOne.equalChars('a', 'a'));
+        assertFalse(offByOne.equalChars('a', 'A'));
     }
 }
