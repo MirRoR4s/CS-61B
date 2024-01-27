f098d76c8c4d4b9e506ea6e261f81ab8ed232c0a
MirRoR4s <790356373@qq.com>
2024-01-27 17:51:14 +0800

    a class that define a equalChars() method and consider two chars is equal if their offset equals one integer.

---
+++ proj1b/OffByN.java
@@ -0,0 +1,13 @@
+public class OffByN implements CharacterComparator {
+    private int offset;
+    
+    public OffByN(int N) {
+        offset = N;
+    }
+
+    @Override
+    public boolean equalChars(char x, char y) {
+        return Math.abs(x - y) == offset;
+    }
+    
+}
