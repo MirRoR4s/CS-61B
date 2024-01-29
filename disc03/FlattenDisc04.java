201dc9f105bb10653f7975be289bc6b106c42759
MirRoR4s <790356373@qq.com>
2024-01-29 21:49:32 +0800

    finished disc03

---
+++ disc03/FlattenDisc04.java
@@ -0,0 +1,24 @@
+public class FlattenDisc04 {
+    /**
+     * 将二维数组x中的元素串联起来并返回
+     * @param x 二维数组
+     * @return 包含串联元素的一维数组
+     */
+    public static int[] flatten(int[][] x) {
+        // 遍历二维数组求总长度
+        int totalLength = 0;
+        for (int i = 0; i < x.length; i++) {
+            totalLength += x[i].length;
+        }
+        // 根据总长度定义新数组
+        int[] a =  new int[totalLength];
+        int aIndex = 0;
+        for (int i = 0; i < x.length; i++) {
+            for (int j = 0; j < x[i].length; j++) {
+                a[aIndex] = x[i][j];
+                aIndex++;
+            }
+        }
+        return a;
+    }
+}
