abb6ad8f2aadab34fbe32702ad247a7cf02f792c
MirRoR4s <790356373@qq.com>
2024-02-01 10:02:18 +0800

    删除无用输出语句

--- proj1a/LinkedListDequeTest.java
+++ proj1a/LinkedListDequeTest.java
@@ -84,9 +84,6 @@ public class LinkedListDequeTest {
      * && is the "and" operation.
      */
     public static void addIsEmptySizeTest() {
-        System.out.println("Running add/isEmpty/Size test.");
-        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-
         LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
 
         boolean passed = checkEmpty(true, lld1.isEmpty());
@@ -115,11 +112,6 @@ public class LinkedListDequeTest {
      * Adds an item, then removes an item, and ensures that dll is empty afterwards.
      */
     public static void addRemoveTest() {
-
-        System.out.println("Running add/remove test.");
-
-        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-
         LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
         // should be empty
         boolean passed = checkEmpty(true, lld1.isEmpty());
