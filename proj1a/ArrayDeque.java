b33f9f63b8d242f6d2fffd1059fc55d538c0c9ef
MirRoR4s <790356373@qq.com>
2024-02-01 09:19:29 +0800

    改进代码格式，去除tab制表符。优化出队时的代码逻辑

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -5,7 +5,7 @@
  * 3. 常量时间获取长度
  * 4. 数组的起始长度为 8
  * 5. 程序所用内存与项数成正比，对于长度 16 以上的数组，其使用率至少应该在 25 % 以上
- * 
+ *
  * @author 黄建涛
  */
 
@@ -13,7 +13,7 @@ public class ArrayDeque<T> {
     // 指向队首元素的位置
     private int front;
     // 指向队尾元素的下一个位置
-    private int back;
+    private int backNext;
     private T[] items;
     private int size;
 
@@ -21,12 +21,12 @@ public class ArrayDeque<T> {
         items = (T[]) new Object[8];
         size = 0;
         front = 0;
-        back = 0;
+        backNext = 0;
     }
 
     /**
      * 修改数组的总长度
-     * 
+     *
      * @param cap 修改后数组的总长度
      */
     private void reSize(int cap) {
@@ -36,12 +36,12 @@ public class ArrayDeque<T> {
         }
         items = newArray;
         front = 0;
-        back = size;
+        backNext = size;
     }
 
     /**
      * 将 item 置于队首
-     * 
+     *
      * @param item
      */
     public void addFirst(T item) {
@@ -55,39 +55,26 @@ public class ArrayDeque<T> {
 
     /**
      * 将 item 置于队尾
-     * 
+     *
      * @param item
      */
     public void addLast(T item) {
         if (size == items.length) {
             reSize(items.length * 2);
         }
-        items[back] = item;
-        back = (back + 1) % items.length;
+        items[backNext] = item;
+        backNext = (backNext + 1) % items.length;
         size += 1;
     }
 
-    /**
-     * 判断队列是否为空
-     * 
-     * @return 为空返回 true，否则返回 false
-     */
     public boolean isEmpty() {
         return size == 0;
     }
 
-    /**
-     * 获取队列大小
-     * 
-     * @return 队列大小
-     */
     public int size() {
         return size;
     }
 
-    /**
-     * 以空格为分隔符从头至尾打印队列
-     */
     public void printDeque() {
         for (int i = 0; i < size; i++) {
             if (i != 0) {
@@ -98,54 +85,38 @@ public class ArrayDeque<T> {
         }
     }
 
-    /**
-     * 删除并返回队首元素
-     * 
-     * @return 原队首元素
-     */
     public T removeFirst() {
-        if (size > 0) {
-            T tmp = items[front];
-            front = (front + 1) % items.length;
-            size = size - 1;
-            double usage = (double) size / items.length;
-            if (items.length >= 16 && usage < 0.25) {
-                reSize(items.length / 2);
-            }
-            return tmp;
+        if (size == 0) {
+            return null;
         }
-        return null;
+        T tmp = items[front];
+        front = (front + 1) % items.length;
+        size = size - 1;
+        double usage = (double) size / items.length;
+        if (items.length >= 16 && usage < 0.25) {
+            reSize(items.length / 2);
+        }
+        return tmp;
     }
 
-    /**
-     * 删除并返回队尾元素
-     * 
-     * @return 原队尾元素
-     */
     public T removeLast() {
-        if (size > 0) {
-            // 最后一个元素的索引
-            int lastIndex = (back - 1 + items.length) % items.length;
-            T tmp = items[lastIndex];
-            // 删除最后一个元素
-            items[lastIndex] = null;
-            back = lastIndex;
-            size = size - 1;
-            double usage = (double) size / items.length;
-            if (usage < 0.25 && items.length >= 16) {
-                reSize(items.length / 2);
-            }
-            return tmp;
+        if (size == 0) {
+            return null;
         }
-        return null;
+        // 最后一个元素的索引
+        int lastIndex = (backNext - 1 + items.length) % items.length;
+        T tmp = items[lastIndex];
+        // 删除最后一个元素
+        items[lastIndex] = null;
+        backNext = lastIndex;
+        size = size - 1;
+        double usage = (double) size / items.length;
+        if (usage < 0.25 && items.length >= 16) {
+            reSize(items.length / 2);
+        }
+        return tmp;
     }
 
-    /**
-     * 获取指定 index 索引处的元素
-     * 
-     * @param index 索引
-     * @return index 索引处对应的队列元素
-     */
     public T get(int index) {
         return items[(front + index) % items.length];
     }
--- proj1a/LinkedListDequeTest.java
+++ proj1a/LinkedListDequeTest.java
@@ -1,135 +1,144 @@
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
-/** Performs some basic linked list tests. */
+
+/**
+ * Performs some basic linked list tests.
+ */
 public class LinkedListDequeTest {
-	@Test
-	public void testRemoveLast() {
-		LinkedListDeque<Integer> lld1 = new LinkedListDeque<>();
-		lld1.addLast(1);
-		int ans = lld1.removeLast();
-		assertEquals(1, ans);
-		assertEquals(lld1.size(), 0);
-		lld1.addLast(1);
-		lld1.addLast(2);
-		ans = lld1.removeLast();
-		assertEquals(2, ans);
-		assertEquals(lld1.size(), 1);
-	}
+    @Test
+    public void testRemoveLast() {
+        LinkedListDeque<Integer> lld1 = new LinkedListDeque<>();
+        lld1.addLast(1);
+        int ans = lld1.removeLast();
+        assertEquals(1, ans);
+        assertEquals(lld1.size(), 0);
+        lld1.addLast(1);
+        lld1.addLast(2);
+        ans = lld1.removeLast();
+        assertEquals(2, ans);
+        assertEquals(lld1.size(), 1);
+    }
 
-	@Test
-	public void testGet() {
-		LinkedListDeque<Integer> lld1 = new LinkedListDeque<>();
-		lld1.addLast(1);
-		lld1.addLast(2);
-		int ans = lld1.get(0);
-		assertEquals(1, ans);
-		ans = lld1.get(1);
-		assertEquals(2, ans);
-		lld1.addFirst(3);
-		ans = lld1.get(0);
-		assertEquals(3, ans);
-	}
+    @Test
+    public void testGet() {
+        LinkedListDeque<Integer> lld1 = new LinkedListDeque<>();
+        lld1.addLast(1);
+        lld1.addLast(2);
+        int ans = lld1.get(0);
+        assertEquals(1, ans);
+        ans = lld1.get(1);
+        assertEquals(2, ans);
+        lld1.addFirst(3);
+        ans = lld1.get(0);
+        assertEquals(3, ans);
+    }
 
-	@Test
-	public void testGetRecursive() {
-		LinkedListDeque<Integer> lld1 = new LinkedListDeque<>();
-		lld1.addLast(1);
-		lld1.addLast(2);
-		int ans = lld1.getRecursive(0);
-		assertEquals(1, ans);
-		ans = lld1.getRecursive(1);
-		assertEquals(2, ans);
-		lld1.addFirst(3);
-		ans = lld1.getRecursive(0);
-		assertEquals(3, ans);
-	}
-	/* Utility method for printing out empty checks. */
-	public static boolean checkEmpty(boolean expected, boolean actual) {
-		if (expected != actual) {
-			System.out.println("isEmpty() returned " + actual + ", but expected: " + expected);
-			return false;
-		}
-		return true;
-	}
+    @Test
+    public void testGetRecursive() {
+        LinkedListDeque<Integer> lld1 = new LinkedListDeque<>();
+        lld1.addLast(1);
+        lld1.addLast(2);
+        int ans = lld1.getRecursive(0);
+        assertEquals(1, ans);
+        ans = lld1.getRecursive(1);
+        assertEquals(2, ans);
+        lld1.addFirst(3);
+        ans = lld1.getRecursive(0);
+        assertEquals(3, ans);
+    }
 
-	/* Utility method for printing out empty checks. */
-	public static boolean checkSize(int expected, int actual) {
-		if (expected != actual) {
-			System.out.println("size() returned " + actual + ", but expected: " + expected);
-			return false;
-		}
-		return true;
-	}
+    /* Utility method for printing out empty checks. */
+    public static boolean checkEmpty(boolean expected, boolean actual) {
+        if (expected != actual) {
+            System.out.println("isEmpty() returned " + actual + ", but expected: " + expected);
+            return false;
+        }
+        return true;
+    }
 
-	/* Prints a nice message based on whether a test passed. 
-	 * The \n means newline. */
-	public static void printTestStatus(boolean passed) {
-		if (passed) {
-			System.out.println("Test passed!\n");
-		} else {
-			System.out.println("Test failed!\n");
-		}
-	}
+    /* Utility method for printing out empty checks. */
+    public static boolean checkSize(int expected, int actual) {
+        if (expected != actual) {
+            System.out.println("size() returned " + actual + ", but expected: " + expected);
+            return false;
+        }
+        return true;
+    }
 
-	/** Adds a few things to the list, checking isEmpty() and size() are correct, 
-	  * finally printing the results. 
-	  *
-	  * && is the "and" operation. */
-	public static void addIsEmptySizeTest() {
-		System.out.println("Running add/isEmpty/Size test.");
-		System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-		
-		LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
+    /* Prints a nice message based on whether a test passed.
+     * The \n means newline. */
+    public static void printTestStatus(boolean passed) {
+        if (passed) {
+            System.out.println("Test passed!\n");
+        } else {
+            System.out.println("Test failed!\n");
+        }
+    }
 
-		boolean passed = checkEmpty(true, lld1.isEmpty());
+    /**
+     * Adds a few things to the list, checking isEmpty() and size() are correct,
+     * finally printing the results.
+     * <p>
+     * && is the "and" operation.
+     */
+    public static void addIsEmptySizeTest() {
+        System.out.println("Running add/isEmpty/Size test.");
+        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
 
-		lld1.addFirst("front");
-		
-		// The && operator is the same as "and" in Python.
-		// It's a binary operator that returns true if both arguments true, and false otherwise.
-		passed = checkSize(1, lld1.size()) && passed;
-		passed = checkEmpty(false, lld1.isEmpty()) && passed;
+        LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
 
-		lld1.addLast("middle");
-		passed = checkSize(2, lld1.size()) && passed;
+        boolean passed = checkEmpty(true, lld1.isEmpty());
 
-		lld1.addLast("back");
-		passed = checkSize(3, lld1.size()) && passed;
+        lld1.addFirst("front");
 
-		System.out.println("Printing out deque: ");
-		lld1.printDeque();
+        // The && operator is the same as "and" in Python.
+        // It's a binary operator that returns true if both arguments true, and false otherwise.
+        passed = checkSize(1, lld1.size()) && passed;
+        passed = checkEmpty(false, lld1.isEmpty()) && passed;
 
-		printTestStatus(passed);
-		
-	}
+        lld1.addLast("middle");
+        passed = checkSize(2, lld1.size()) && passed;
 
-	/** Adds an item, then removes an item, and ensures that dll is empty afterwards. */
-	public static void addRemoveTest() {
+        lld1.addLast("back");
+        passed = checkSize(3, lld1.size()) && passed;
 
-		System.out.println("Running add/remove test.");
+        System.out.println("Printing out deque: ");
+        lld1.printDeque();
 
-		System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-		
-		LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
-		// should be empty 
-		boolean passed = checkEmpty(true, lld1.isEmpty());
+        printTestStatus(passed);
 
-		lld1.addFirst(10);
-		// should not be empty 
-		passed = checkEmpty(false, lld1.isEmpty()) && passed;
+    }
 
-		lld1.removeFirst();
-		// should be empty 
-		passed = checkEmpty(true, lld1.isEmpty()) && passed;
+    /**
+     * Adds an item, then removes an item, and ensures that dll is empty afterwards.
+     */
+    public static void addRemoveTest() {
 
-		printTestStatus(passed);
-		
-	}
+        System.out.println("Running add/remove test.");
 
-	public static void main(String[] args) {
-		System.out.println("Running tests.\n");
-		addIsEmptySizeTest();
-		addRemoveTest();
-	}
-} 
+        System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
+
+        LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
+        // should be empty
+        boolean passed = checkEmpty(true, lld1.isEmpty());
+
+        lld1.addFirst(10);
+        // should not be empty
+        passed = checkEmpty(false, lld1.isEmpty()) && passed;
+
+        lld1.removeFirst();
+        // should be empty
+        passed = checkEmpty(true, lld1.isEmpty()) && passed;
+
+        printTestStatus(passed);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Running tests.\n");
+        addIsEmptySizeTest();
+        addRemoveTest();
+    }
+}
