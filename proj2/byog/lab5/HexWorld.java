052cf5d23e35601cea5ebf4ec0e391281c1acd8a
MirRoR4s <790356373@qq.com>
2024-02-13 08:38:23 +0800

    代码格式规范化

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -17,47 +17,45 @@ public class HexWorld {
             this.y = y;
         }
     }
-    private static final int WIDTH = 50;
-    private static final int HEIGHT = 50;
 
-    public TERenderer initial(int width, int height) {
-        TERenderer ter = new TERenderer();
-        ter.initialize(width, height);
-        return ter;
+    /** 绘制一个六边形 */
+    public static void addHexagon(TETile[][] world, Position p, int s, TETile t) {
+        TERenderer teRenderer = new TERenderer();
+        initial(teRenderer, world);
+        fillWithHexagon(world, p, s, t);
+        teRenderer.renderFrame(world);
     }
 
-    public void initialFill(TETile[][] world) {
-        for (int x = 0; x < WIDTH; x += 1) {
-            for (int y = 0; y < HEIGHT; y += 1) {
+    private static void initial(TERenderer teRenderer, TETile[][] world) {
+        teRenderer.initialize(world.length, world[0].length);
+        initialFill(world);
+    }
+
+    /**初始时将世界全部填黑*/
+    private static void initialFill(TETile[][] world) {
+        for (int x = 0; x < world.length; x += 1) {
+            for (int y = 0; y < world[0].length; y += 1) {
                 world[x][y] = Tileset.NOTHING;
             }
         }
     }
 
-    /** draw a hexagon in p.x and p.y with side length equal s*/
-    public static void addHexagon(TETile[][] world, Position p, int s, TETile t) {
-        fillWithHexagon(world, p, s, t);
-    }
-
     /** 绘制一行瓷砖 */
-    private static void drawTile(TETile[][] world, Position p, TETile t, int sideLength, int nothingNum) {
-            for (int i = 0; i < nothingNum; i++) {
-                p.x += 1;
-            }
+    private static void drawRowTile(TETile[][] world, Position p, TETile t, int sideLength, int nothingNum) {
+            p.x += nothingNum;
             for (int i = 0; i < sideLength; i++) {
                 world[p.x][p.y] = t;
                 p.x += 1;
             }
-            for (int i = 0; i < nothingNum; i++) {
-                p.x += 1;
-            }
+            p.x += nothingNum;
     }
 
-
+    /**计算六边形最大边长（最宽的部分）*/
     private static int calcMaxSideLength(int s) {
         return 3 * s - 2;
     }
 
+    /**计算绘制六边形所需总行数*/
     private static int calculateTotalRowNum(int s) {
         return 2 * s;
     }
@@ -66,6 +64,7 @@ public class HexWorld {
         return (maxSideLength - currentSideLength) / 2;
     }
 
+    /** 填充六边形 */
     private static void fillWithHexagon(TETile[][] world, Position p, int s, TETile t) {
         int maxSideLength = calcMaxSideLength(s);
         int rowNum = calculateTotalRowNum(s);
@@ -78,28 +77,52 @@ public class HexWorld {
                 delta = -2;
             }
             int nothingNum = calcNothingNum(maxSideLength, currentSideLenth);
-            drawTile(world, p, t, currentSideLenth, nothingNum);
+            drawRowTile(world, p, t, currentSideLenth, nothingNum);
             p.x = x;
             p.y += 1;
             currentSideLenth += delta;
         }
     }
-
+    /** 绘制一个六边形世界（总共 19 个六边形） */
     public static void drawHexagonWorld(TETile[][] world, Position p, int s, TETile t) {
-                Position[] positions1 = calcPositionCol1(p, s);
-                for (Position p1: positions1) {
-                    addHexagon(world, p1, s, t);
-            }
-                positions1 = calcPositionCol2(p, s);
-                for (Position p2: positions1) {
-                    addHexagon(world, p2, s, Tileset.FLOWER);
-                }
+        TERenderer teRenderer = new TERenderer();
+        initial(teRenderer, world);
+        int maxSideLength = calcMaxSideLength(s);
+        int deltaX = maxSideLength - 2;
+        int deltaY = 3;
+
+        Position[] PositionCol1 = calcPosition(p, s, 3, p.x, p.y);
+        Position[] PositionCol2 = calcPosition(p, s, 4, p.x + deltaX, p.y - deltaY);
+        Position[] PositionCol3 = calcPosition(p, s, 5, p.x + (2 * deltaX), p.y - (2 * deltaY));
+        Position[] PositionCol4 = calcPosition(p, s, 4, p.x + (3 * deltaX), p.y - deltaY);
+        Position[] PositionCol5 = calcPosition(p, s, 3, p.x + (4 * deltaX), p.y);
+        for (Position p1: PositionCol1) {
+            fillWithHexagon(world, p1, s, t);
         }
+        for (Position p2: PositionCol2) {
+            fillWithHexagon(world, p2, s, Tileset.FLOWER);
+        }
+        for (Position p3: PositionCol3) {
+            fillWithHexagon(world, p3, s, Tileset.MOUNTAIN);
+        }
+        for (Position p4: PositionCol4) {
+            fillWithHexagon(world, p4, s, Tileset.GRASS);
+        }
+        for (Position p5: PositionCol5) {
+            fillWithHexagon(world, p5, s, Tileset.SAND);
+        }
+        teRenderer.renderFrame(world);
 
-//    private static Position[] caclPosition(Position p, int s) {
-//        Position[] positions = new Position[7];
-//    }
-
+    }
+    private static Position[] calcPosition(Position p, int sideLength, int hexagonNum, int startX, int startY) {
+        Position[] positions = new Position[hexagonNum];
+        positions[0] = new Position(startX, startY);
+        for (int i = 1; i < hexagonNum; i++) {
+            startY += calculateTotalRowNum(sideLength);
+            positions[i] = new Position(startX, startY);
+        }
+        return positions;
+    }
     private static Position[] calcPositionCol1(Position p, int s) {
         Position[] positions = new Position[3];
         int startX = p.x;
@@ -125,17 +148,14 @@ public class HexWorld {
         return positions;
     }
 
-
     public static void main(String[] args) {
         TETile[][] world = new TETile[50][50];
-        HexWorld hw = new HexWorld();
-        TERenderer teRenderer = hw.initial(50, 50);
-        hw.initialFill(world);
         int startX = 0;
         int startY = 20;
         int sideLength = 3;
-        Position p = new Position(0, 20);
-        int maxSideLength = calcMaxSideLength(sideLength);
+        Position p = new Position(startX, startY);
+//        addHexagon(world, p, sideLength, Tileset.FLOWER);
+//        int maxSideLength = calcMaxSideLength(sideLength);
 //        Position p = new Position(startX, startY);
 //        addHexagon(world, p, 3, Tileset.FLOWER);
 //        Position p1 = new Position(startX, startY + 6);
@@ -147,7 +167,6 @@ public class HexWorld {
 //        addHexagon(world, p3, 3, Tileset.WALL);
 //        teRenderer.renderFrame(world);
         drawHexagonWorld(world, p, sideLength, Tileset.TREE);
-        teRenderer.renderFrame(world);
 
     }
 
