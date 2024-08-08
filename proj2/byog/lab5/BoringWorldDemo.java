d43f1514d3f95ee27982772f0dd2c4c32214b33f
MirRoR4s <790356373@qq.com>
2024-08-08 19:01:33 +0800

    lab5回顾

--- proj2/byog/lab5/BoringWorldDemo.java
+++ proj2/byog/lab5/BoringWorldDemo.java
@@ -5,35 +5,37 @@ import byog.TileEngine.TETile;
 import byog.TileEngine.Tileset;
 
 /**
- *  Draws a world that is mostly empty except for a small region.
+ * Draws a world that is mostly empty except for a small region.
  */
 public class BoringWorldDemo {
-    private static final int WIDTH = 60;
-    private static final int HEIGHT = 30;
+    private static final int WIDTH = 60; // 60 瓦片 tile 宽
+    private static final int HEIGHT = 30; // 30 瓦片高
 
     public static void main(String[] args) {
         // initialize the tile rendering engine with a window of size WIDTH x HEIGHT
         TERenderer ter = new TERenderer();
-        ter.initialize(WIDTH, HEIGHT);
+        ter.initialize(WIDTH, HEIGHT); // 定义一个 WIDTH 瓦片宽，HEIGHT 瓦片高的世界，注意到一个瓦片是 16 * 16 像素
 
         // initialize tiles
+        // 给瓦片世界的每个瓦片一个默认样式，NOTHING 意味着全黑
         TETile[][] world = new TETile[WIDTH][HEIGHT];
         for (int x = 0; x < WIDTH; x += 1) {
             for (int y = 0; y < HEIGHT; y += 1) {
                 world[x][y] = Tileset.NOTHING;
+                // world[x][y] = Tileset.FLOWER;
             }
         }
-        world[0][0] = Tileset.WALL;
+
         // fills in a block 14 tiles wide by 4 tiles tall
-//        for (int x = 21; x < 35; x += 1) {
-//            for (int y = 5; y < 10; y += 1) {
-//                world[x][y] = Tileset.WALL;
-//            }
-//        }
+        // 给瓦片世界的部分区域增添光彩。
+        for (int x = 21; x < 35; x += 1) {
+            for (int y = 5; y < 10; y += 1) {
+                world[x][y] = Tileset.WALL;
+            }
+        }
 
         // draws the world to the screen
-        ter.renderFrame(world);
+        ter.renderFrame(world); // 渲染瓦片，这样在屏幕上就能看到它们了。
     }
-
 
 }
