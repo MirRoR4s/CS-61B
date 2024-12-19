65fe1c8e552a5f76d875f10ef245632b9d60c5fe
MirRoR4s <790356373@qq.com>
2024-12-19 21:17:14 +0800

    基于A*算法求解了迷宫路径问题

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,13 +1,16 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.IndexMinPQ;
+
 /**
- *  @author Josh Hug
+ * @author Josh Hug
  */
 public class MazeAStarPath extends MazeExplorer {
     private int s;
     private int t;
     private boolean targetFound = false;
     private Maze maze;
+    private IndexMinPQ<Integer> fringe;
 
     public MazeAStarPath(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
@@ -16,11 +19,27 @@ public class MazeAStarPath extends MazeExplorer {
         t = maze.xyTo1D(targetX, targetY);
         distTo[s] = 0;
         edgeTo[s] = s;
+        fringe = new IndexMinPQ<>(maze.V());
+        insertAllVertices();
     }
 
     /** Estimate of the distance from v to the target. */
     private int h(int v) {
-        return -1;
+        // 以曼哈顿距离作为 h(v)
+        return Math.abs(maze.toX(v) - maze.toX(t)) + Math.abs(maze.toY(v) - maze.toY(t));
+    }
+
+    /**
+     * 将所有顶点以 d(source, v) + h(v, goal) 为排序依据加入优先队列中。
+     */
+    private void insertAllVertices() {
+        for (int i = 0; i < maze.V(); i++) {
+            if (distTo[i] == Integer.MAX_VALUE) {
+                fringe.insert(i, distTo[i]);
+            } else {
+                fringe.insert(i, distTo[i] + h(i));
+            }
+        }
     }
 
     /** Finds vertex estimated to be closest to target. */
@@ -31,7 +50,42 @@ public class MazeAStarPath extends MazeExplorer {
 
     /** Performs an A star search from vertex s. */
     private void astar(int s) {
-        // TODO
+        // 借助普林斯顿算法包提供的相关API实现A星算法
+        while (!fringe.isEmpty()) {
+            if (targetFound) {
+                return;
+            }
+
+            int v = fringe.delMin();
+            for (int e : maze.adj(v)) {
+                relax(v, e, 1);
+            }
+        }
+
+    }
+
+    /**
+     * 松弛以v为起点，w为终点的边
+     * 
+     * @param v      边的起点
+     * @param w      边的终点
+     * @param weight 边的权重
+     */
+    private void relax(int v, int w, int weight) {
+        if (distTo[w] > distTo[v] + weight) {
+            distTo[w] = distTo[v] + weight;
+            edgeTo[w] = v;
+            marked[v] = true;
+            marked[w] = true;
+            announce();
+            if (fringe.contains(w)) {
+                fringe.decreaseKey(w, distTo[w]);
+            }
+        }
+
+        if (w == t) {
+            targetFound = true;
+        }
     }
 
     @Override
@@ -40,4 +94,3 @@ public class MazeAStarPath extends MazeExplorer {
     }
 
 }
-
--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -20,6 +20,7 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     private Queue<Integer> q;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
+        // Add more variables here!
         super(m);
         maze = m;
         s = maze.xyTo1D(sourceX, sourceY);
@@ -28,11 +29,12 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
         edgeTo[s] = s;
         q = new LinkedList<>();
         q.add(s);
-        // Add more variables here!
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
+        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as
+        // well as call announce()
         while (!q.isEmpty()) {
             int v = q.remove();
             marked[v] = true;
@@ -55,8 +57,6 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
                 }
             }
         }
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as
-        // well as call announce()
     }
 
     @Override
